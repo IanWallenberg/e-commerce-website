bb40433dd02baf6c5636dc8db38b8244e5a15e97
Santiago <santichiste@hotmail.com>
2022-04-12 13:55:13 +0200

    added prettier

--- client/src/routers/EcommerceRouter.js
+++ client/src/routers/EcommerceRouter.js
@@ -1,18 +1,13 @@
-import React from 'react'
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from "react-router-dom";
-import { LandingPage } from '../components/LandingPage/LandingPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LandingPage } from "../components/LandingPage/LandingPage";
 
 export const EcommerceRouter = () => {
   return (
     <Router>
-        <Routes>
-            <Route path="/home" element={<LandingPage />}/>
-
-        </Routes>
+      <Routes>
+        <Route path="/home" element={<LandingPage />} />
+      </Routes>
     </Router>
-  )
-}
+  );
+};
