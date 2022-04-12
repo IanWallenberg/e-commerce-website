d0d09a42efb0d67b3f5c37b9a1c786e0b046c47c
Santiago <santichiste@hotmail.com>
2022-04-12 23:58:03 +0200

    updated route to checkout cart

--- client/src/routers/EcommerceRouter.js
+++ client/src/routers/EcommerceRouter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CheckoutPage } from "../components/Checkout/CartPage/CheckoutPageCart";
 import { LandingPage } from "../components/LandingPage/LandingPage";
 
 export const EcommerceRouter = () => {
@@ -7,6 +8,7 @@ export const EcommerceRouter = () => {
     <Router>
       <Routes>
         <Route path="/home" element={<LandingPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
       </Routes>
     </Router>
   );
