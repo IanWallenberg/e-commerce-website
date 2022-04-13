136fb002ef929acfd0e7825f28b540e7693d8474
IanWallenberg <90833604+IanWallenberg@users.noreply.github.com>
2022-04-13 10:02:30 +0200

    first commit

--- client/src/routers/EcommerceRouter.js
+++ client/src/routers/EcommerceRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CheckoutPage } from "../components/Checkout/CartPage/CheckoutPageCart";
 import { LandingPage } from "../components/LandingPage/LandingPage";
+import { LoginScreen } from "../components/Registration/LoginScreen";
 
 export const EcommerceRouter = () => {
   return (
@@ -9,6 +10,7 @@ export const EcommerceRouter = () => {
       <Routes>
         <Route path="/home" element={<LandingPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/login" element={<LoginScreen />} />
       </Routes>
     </Router>
   );
