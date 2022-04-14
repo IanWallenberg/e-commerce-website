5339317eaf9fc026e328d3dd3c4b2aedc44f42d8
Sudha Madhuri <sudha.poojari@hyperisland.se>
2022-04-14 14:55:08 +0200

    Created NavBar and Category Page

---
+++ client/src/components/ui/Nav.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import React from "react";
+import { Badge } from "@mui/material";
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import ProfileIcon from '@mui/icons-material/PermIdentity';
+import Image from '../Categories/Img/logo.png';
+
+const Container = styled.div`
+height:10%;
+background-color: #F8EDEB;`
+
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+justify-content: space-between;
+align-items:center;`
+
+const Left = styled.div`
+width:50%;
+align-items:center;`
+
+
+const Logo = styled.div`
+width: 60px;
+height: 40px;
+display: flex;
+align-items:center;
+justify-items: center;
+`;
+
+
+const Right= styled.div`
+display:flex; 
+align-items:center; 
+justify-content:flex-end;`
+
+const MenuItem = styled.div`
+font-size: 14px; 
+cursor: pointer;
+margin-left:25px;`
+
+
+const Nav = () => {
+    return ( 
+        <Container> 
+        <Wrapper>
+            <Left>
+                <Logo >
+                    <img src = {Image} className="w-100"  alt ="logo"></img>
+                </Logo>
+                
+            </Left>
+            <Right> 
+                    <MenuItem> <SearchIcon/> </MenuItem>
+                    <MenuItem> <ProfileIcon/> </MenuItem>
+                    <MenuItem> 
+                        <Badge badgeContent={4} color="primary">
+                        <ShoppingCartOutlinedIcon  />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+
+        </Wrapper>
+        </Container>
+     );
+}
+ 
+export default Nav;
--- client/src/routers/EcommerceRouter.js
+++ client/src/routers/EcommerceRouter.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LandingPage } from "../components/LandingPage/LandingPage";
+import {CategoriesPage} from '../components/Categories/CategoriesPage';
 
 export const EcommerceRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+      <Routes>
+        <Route path="/categories" element={<CategoriesPage />} />
       </Routes>
     </Router>
   );
