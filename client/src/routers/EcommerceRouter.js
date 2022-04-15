c435e38acf7ceec58212d2f0652696bbf7c386c3
Santiago <santichiste@hotmail.com>
2022-04-15 18:10:56 +0200

    added proper routes and the top design to each checkout website

--- client/src/routers/EcommerceRouter.js
+++ client/src/routers/EcommerceRouter.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CheckoutPage } from "../components/Checkout/CartPage/CheckoutPageCart";
+import { CheckoutCart } from "../components/Checkout/CartPage/CheckoutCart";
+import { CheckoutDelivery } from "../components/Checkout/DeliveryPage/CheckoutDelivery";
+import { CheckoutPayment } from "../components/Checkout/PaymentPage/CheckoutPayment";
 import { LandingPage } from "../components/LandingPage/LandingPage";
 import { LoginScreen } from "../components/Registration/LoginScreen";
 
@@ -9,9 +11,13 @@ export const EcommerceRouter = () => {
     <Router>
       <Routes>
         <Route path="/home" element={<LandingPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/checkout-Cart" element={<CheckoutCart />} />
+        <Route path="/checkout-Delivery" element={<CheckoutDelivery />} />
+        <Route path="/checkout-Payment" element={<CheckoutPayment />} />
         <Route path="/login" element={<LoginScreen />} />
       </Routes>
     </Router>
   );
 };
+
+
