056fe5b32f6a3a4b312a12175dadf0b27aa69f85
Santiago <santichiste@hotmail.com>
2022-04-19 22:09:57 +0200

    finished design delivery page

--- client/src/components/Checkout/DeliveryPage/CheckoutDelivery.js
+++ client/src/components/Checkout/DeliveryPage/CheckoutDelivery.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTruck } from "@fortawesome/free-solid-svg-icons";
+import "./styles/checkoutDelivery.css";
 
 export const CheckoutDelivery = () => {
   return (
@@ -46,6 +47,65 @@ export const CheckoutDelivery = () => {
       </div>
       <div className="container text-center">
         <FontAwesomeIcon icon={faTruck} className="fs-1 p-5" />
+      </div>
+      {/* CONTAINER BEGINS */}
+      <div className="container border rounded shadow-lg">
+        <h1 id="title__Delivery" className="text-center fw-light mt-3">
+          DELIVERY
+        </h1>
+        <h5 className="text-center mt-4 fw-bold"> ENTER YOUR E-MAIL</h5>
+        <div className="d-flex justify-content-center mb-3">
+          <input
+            className="form-control form-control-lg w-50 justify-content-center "
+            placeholder=""
+            type="email"
+          ></input>
+        </div>
+
+        <div className="container d-flex flex-wrap justify-content-between">
+          <div className="column col-5 me-5">
+            <h5 id="title__Delivery " className="fw-bold mb-3">
+              DETAILS
+            </h5>
+            <p>
+              First Name<strong className="">*</strong>
+            </p>
+            <input className="form-control form-control-lg"></input>
+            <p>
+              Last Name<strong>*</strong>
+            </p>
+            <input className="form-control form-control-lg"></input>
+            <p>Company Name</p>
+            <input className="form-control form-control-lg"></input>
+            <p>Phone Number</p>
+            <input className="form-control form-control-lg"></input>
+          </div>
+          <div className="column col-5 ms-5">
+            <h5 id="title__Delivery " className="fw-bold mb-3">
+              SHIPPING
+            </h5>
+            <p>
+              Street Adress<strong>*</strong>
+            </p>
+            <input className="form-control form-control-lg"></input>
+            <p>
+              City<strong>*</strong>
+            </p>
+            <input className="form-control form-control-lg"></input>
+            <p>
+              ZIP Code<strong>*</strong>
+            </p>
+            <input className="form-control form-control-lg"></input>
+          </div>
+          <div className="container d-flex justify-content-center mt-5">
+            <button
+              id="proceed__Button"
+              className="text-center btn btn-dark btn-lg shadow w-25 border-white mb-5"
+            >
+              PROCEED
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
