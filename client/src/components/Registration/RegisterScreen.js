bb93de63d0552e0cc500609064f7c84062330a0f
Oliver Hegardt <90907842+oliverhegardt@users.noreply.github.com>
2022-04-19 10:58:06 +0200

    updated stylecomponents and padding

--- client/src/components/Registration/RegisterScreen.js
+++ client/src/components/Registration/RegisterScreen.js
@@ -1,17 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../Registration/Register.css";
 
-
-
 const Background = styled.html`
-    background-color: #F8EDEB;
-    min-height: 100vh;
-    min-width: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;`
+  background-color: #f8edeb;
+  min-height: 100vh;
+  min-width: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Flex = styled.div`
 // display: flex;
@@ -22,7 +21,7 @@ background-color: white;
   padding: 50px;
   width: 500px;
   height 550px;
-`
+`;
 
 // const Box = styled.div`
 //   color: black;
@@ -36,68 +35,71 @@ const Title = styled.h1`
   font-size: 2em;
   color: black;
   // text-align: center;
-  `
+`;
 
-  const Form = styled.input`
+const Form = styled.input`
   background-color: lightgrey;
   padding: 1rem;
   width: 350px;
   height: 10px;
   opacity: 0.7;
   border-radius: 5px;
-  `
+`;
 
-  const Paragraph = styled.p`
+const Paragraph = styled.p`
   color: black;
-  `
+`;
 
-  const DIV = styled.div`
+const DIV = styled.div`
   display: flex;
   justify-content: space-between;
   text-decoration: none;
   padding: 20px;
   font-size: 15px;
-  `
+`;
 
-  const Button = styled.button`
-  width: 150px;
-  height: 50px;
-  color: black;
-  background-color: #FFE5D9;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-  `
-  const ButtonWrapper = styled.div`
+const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   padding-top: 50px;
   padding-left: 120px;
-  `
+`;
 
-  
+const Button = styled.button`
+  width: 150px;
+  height: 50px;
+  color: black;
+  background-color: #ffe5d9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+`;
 
 export const RegisterScreen = () => {
   return (
     <Background>
       <Flex>
-<Title>Create Account</Title>
-    <Paragraph>First name:</Paragraph>
-<Form type="text" name="uname" required />
-<Paragraph>Last name:</Paragraph>
-<Form type="text" name="uname" required /> 
-<Paragraph>Email adress:</Paragraph>
-<Form type="text" name="uname" required />
-<Paragraph>Password:</Paragraph>
-<Form type="text" name="uname" required /> 
-<DIV>
-  <Paragraph>I accept the <Link to="/Terms">Terms</Link></Paragraph><input type="checkbox" checked="unchecked"/>
+        <Title>Create Account</Title>
+        <Paragraph>First name:</Paragraph>
+        <Form type="text" name="uname" required />
+        <Paragraph>Last name:</Paragraph>
+        <Form type="text" name="uname" required />
+        <Paragraph>Email adress:</Paragraph>
+        <Form type="text" name="uname" required />
+        <Paragraph>Password:</Paragraph>
+        <Form type="text" name="uname" required />
+        <DIV>
+          <Paragraph>
+            I accept the <Link to="/Terms">Terms</Link>
+          </Paragraph>
+          <input type="checkbox" checked="unchecked" />
 
-<ButtonWrapper><Button>Create Account</Button></ButtonWrapper>
-</DIV></Flex>
-
-
+          <ButtonWrapper>
+            <Button>Create Account</Button>
+          </ButtonWrapper>
+        </DIV>
+      </Flex>
     </Background>
-  )
-}
+  );
+};
