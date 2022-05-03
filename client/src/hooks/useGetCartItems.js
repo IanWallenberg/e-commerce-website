8a48244249267b752e49f6102dda64ae27aee8b9
Isac Geisewall <isacgeisewall@Isacs-MBP.localdomain>
2022-05-03 11:41:10 +0200

    made changes to displaying data on cart cards

---
+++ client/src/hooks/useGetCartItems.js
@@ -0,0 +1,18 @@
+import { getCartItems } from "../components/helpers/getCartItems";
+
+import { useEffect, useState } from "react";
+
+export const useGetCartItems = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    getCartItems()
+      .then((response) => {
+        return response;
+      })
+      .then((cartItem) => {
+        setCartItems(cartItem);
+      });
+  }, []);
+  return cartItems;
+};
